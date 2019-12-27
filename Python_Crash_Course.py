a85bf3a3254fadd9312cf25394a18b88c1ca40a5
HardboiledEgo <51846859+HardboiledEgo@users.noreply.github.com>
2019-12-27 09:53:41 +0300

    Update Python_Crash_Course.py

--- Python_Crash_Course.py
+++ Python_Crash_Course.py
@@ -131,8 +131,11 @@ sum(digits)
 squares = [value**2 for value in range(1,11)]
 print(squares)
 #
-nums = [nums.insert(0, value) for value in range(1, 11)]
-print(nums)
+squares = [value**2 for value in range(1,11)]
+print(squares)
+#
+numbers = [val for val in range(1, 11)]
+print(numbers)
 #
 nums = []
 for value in range(1, 101):
@@ -142,4 +145,33 @@ print(nums)
 #
 my_list = [1, 2, 3]
 friend_list = my_list[:]#копируется список только срезом, иначе получится клон
+#
+dimensions = (200, 50)#кортеж в круглых скобках(не изменяется), но можно переопределиь весь кортеж в вызове
+print(dimensions[0])
+print(dimensions[1])
+####################
+#Глава 5: команды if
+####################
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+#
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+#
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print("Adding " + requested_topping + ".")
+    else:
+        print("Sorry, we don't have " + requested_topping + ".")
+
+print("\nFinished making your pizza!")
 #
