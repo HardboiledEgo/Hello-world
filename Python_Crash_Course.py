8764110cec9fafcff566df90ceb83ddc0be4641f
HardboiledEgo <51846859+HardboiledEgo@users.noreply.github.com>
2020-01-05 16:02:36 +0300

    Update Python_Crash_Course.py

--- Python_Crash_Course.py
+++ Python_Crash_Course.py
@@ -452,9 +452,11 @@ class Car():
 
 class ElectricCar(Car):
     def __init__(self, make, model, year):
-        super().__init__(self, make, model, year)
+        super().__init__(make, model, year)
 
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer()
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
 #
