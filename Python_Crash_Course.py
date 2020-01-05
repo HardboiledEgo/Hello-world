f04d07287b05794935c8aaf1dd1f067b219c9bf7
HardboiledEgo <51846859+HardboiledEgo@users.noreply.github.com>
2020-01-05 12:56:19 +0300

    Update Python_Crash_Course.py

--- Python_Crash_Course.py
+++ Python_Crash_Course.py
@@ -187,3 +187,224 @@ alien_0['x_position'] = 0
 alien_0['y_position'] = 25
 print(alien_0)
 #
+alien_0 = {'color': 'green', 'points': 5}
+del alien_0['points']
+print alien_0
+#
+favorite_language = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+#
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+    }
+
+for key, value in user_0.items():#обращение key только по ключу словаря, value только по значению, items и по ключу и по значению
+    print("\nKey: " + key)
+    print("Value: " + value)
+#
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " + \
+        language.title() + ".")
+#
+for name in favorite_languages.keys():
+    print(name.title())
+
+for name in favorite_languages:#по умолчанию имеется в виду всегда имя
+    print(name.title())
+#
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+    if name in friends:
+        print(" Hi " + name.title() + ", I see your favorite language is " + \
+            favorite_languages[name].title() + "!")
+#
+print("The following languages have been mentioned:")
+for language in set(favorite_languages.values()):#set проверяет повторы в множестве и выводит только уникальные имена
+    print(language.title())
+#
+aliens = []
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[:5]:
+    print(alien)
+
+print("Total number of aliens: " + str(len(aliens)))
+#
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese'],
+    }
+
+print("You ordered a " + pizza['crust'] + "-crust pizza " + \
+    "with the following toppings:")
+for topping in pizza['toppings']:
+    print("\t" + topping)
+###Глава 7: ввод данных и циклы###
+#
+current_number = 0
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+        continue#игнорирует оставшийся код цикла и запускает некст проход (break прерывает цикл полностью)
+    print(current_number)
+#
+unconfirmed_users = ['alice', 'brian', 'candace']
+unconfirmed_users = sorted(unconfirmed_users, reverse=True)#реверс для красоты вывода, т.к. pop режет список с конца
+confirmed_users = []
+while unconfirmed_users:#пока есть элементы = True
+    current_user = unconfirmed_users.pop()
+    print("Verifying user: " + current_user.title())
+    confirmed_users.append(current_user)
+
+print("\nThe following users have been confirmed:")
+for confirmed_user in confirmed_users:
+    print(confirmed_user.title())
+#
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+while 'cat' in pets:
+    pets.remove('cat')
+
+print(pets)
+#
+responses = {}
+
+polling_active = True
+while polling_active:
+    name = 'name'
+    response = 'everest'
+    responses[name] = response#представление словаря
+    repeat = 'no'
+    if repeat == 'no':
+        polling_active = False
+
+print("\n--- Poll Results ---")
+for name, response in responses.items():
+    print(name + " would like to climb " + response + ".")
+###Глава 8: функции###
+#
+def greet_user():
+    print('Hello World!')
+
+greet_user()
+#
+def describe_pet(pet_name, animal_type='dog'):#использование параметра по умолчанию
+    """Выводит информацию о животном."""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+describe_pet(pet_name='willie')#явное указание имени (можно и не явно т.к. первый аргумент это имя)
+#
+def get_formatted_name(first_name, last_name):
+    """Возвращает аккуратно отформатированное полное имя."""
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+#
+def get_formatted_name(first_name, last_name, middle_name=''):#среднее_имя по умолчанию пусто и игнорируется функцией как значение (bool = False)
+    """Возвращает аккуратно отформатированное полное имя."""
+    if middle_name:
+        full_name = first_name + ' ' + middle_name + ' ' + last_name
+    else:
+        full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+musician = get_formatted_name('john', 'hooker', 'lee')
+print(musician)
+#
+def build_person(first_name, last_name):
+    """Возвращает словарь с информацией о человеке."""
+    person = {'first': first_name, 'last': last_name}
+    return person
+
+musician = build_person('jimi', 'hendrix')
+print(musician)
+#
+def build_person(first_name, last_name, age=''):
+    """Возвращает словарь с информацией о человеке."""
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+
+musician = build_person('jimi', 'hendrix', age=27)
+print(musician)
+#
+def greet_users(names):
+    """Вывод простого приветствия для каждого пользователя в списке."""
+    for name in names:
+        msg = "Hello, " + name.title() + "!"
+        print(msg)
+
+usernames = ['hannah', 'ty', 'margot']
+greet_users(usernames)
+#
+def print_models(unprinted_designs, completed_models):
+    """
+    Имитирует печать моделей, пока список не станет пустым.
+    Каждая модель после печати перемещается в completed_models.
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        # Имитация печати модели на 3D-принтере.
+        print("Printing model: " + current_design)
+        completed_models.append(current_design)#список сохраняется и для внешней переменной вне функции
+
+def show_completed_models(completed_models):
+    """Выводит информацию обо всех напечатанных моделях."""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+print_models(unprinted_designs[:], completed_models)
+show_completed_models(completed_models)
+print(unprinted_designs)
+#
+def make_pizza(size, *toppings):#* позволяет передавать в аргумент кортеж неопределённого размера
+    """Выводит описание пиццы."""
+    print("\nMaking a " + str(size) + \
+        "-inch pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+#
+def build_profile(first, last, **user_info):#** передаёт в аргумент словарь произвольного размера
+    """Строит словарь с информацией о пользователе."""
+    profile = {}
+    profile['first_name'] = first
+    profile['last_name'] = last
+    for key, value in user_info.items():
+        profile[key] = value
+    return profile
+
+user_profile = build_profile('albert', 'einstein', \
+    location='princeton', field='physics')
+print(user_profile)
+#import имя - функции модуля писать через имя.имя_модуля
+#from имя import функция (или * если импорт всего) - функция сохраняет своё имя и пишется без .
+#import имя as другое_имя - присваивание псевдонима
+###Глава 9: классы###
