faeda3c54ec1c788a6b4aec98e22393cb1f445f1
HardboiledEgo <51846859+HardboiledEgo@users.noreply.github.com>
2020-01-05 18:06:28 +0300

    Update Python_Crash_Course.py

--- Python_Crash_Course.py
+++ Python_Crash_Course.py
@@ -453,10 +453,58 @@ class Car():
 class ElectricCar(Car):
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
+        self.battery_size = 70
+    
+    def describe_battery(self):
+        """Выводит информацию о мощности аккумулятора."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
 
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer()
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
+print(my_tesla.describe_battery())
+#
+class Car():
+    """Простая модель автомобиля."""
+    def __init__(self, make, model, year):
+        """Инициализирует атрибуты описания автомобиля."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0#по умолчанию
+    def get_descriptive_name(self):
+        """Возвращает аккуратно отформатированное описание."""
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+    def read_odometer(self):
+        """Выводит пробег машины в милях."""
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+    def update_odometer(self, mileage):#изменение атрибута через метод, иногда можно изменить метод напрямую через присваивание
+        """
+        Устанавливает на одометре заданное значение.
+        При попытке обратной подкрутки изменение отклоняется.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+class Battery():#распухающий код нужно выносить в дополнительные классы, таким образом можно атрибуты внутри класса присваивать другим, не дочерним классам
+    """Простая модель аккумулятора электромобиля."""
+    def __init__(self, battery_size=70):
+        """Инициализирует атрибуты аккумулятора."""
+        self.battery_size = battery_size
+    def describe_battery(self):
+        """Выводит информацию о мощности аккумулятора."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+my_tesla = ElectricCar('tesla', 'model s', 2020)
+my_tesla.battery.describe_battery()#обращение к методу атрибута осуществляется через имя_объекта.имя_атрибута.имя_метода_атрибута
 #
