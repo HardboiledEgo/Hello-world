453daf92f3501e4d3dfccd6e1b4f5d8518e40834
HardboiledEgo <51846859+HardboiledEgo@users.noreply.github.com>
2020-04-07 15:59:36 +0300

    1

--- Python_Crash_Course/test.py
+++ Python_Crash_Course/test.py
@@ -1,16 +1,4 @@
-low_res = (800, 600)
-
-window_resolution = low_res # Разрешение из стандартных
-
-pyxel_pull_x = [x for x in range(0, window_resolution[0], 50)]
-pyxel_pull_y = [y for y in range(0, window_resolution[1], 50)]
-pyxel_pull = []
-
-for x in pyxel_pull_x:
-    for y in pyxel_pull_y:
-            pyxel_pull.append(x)
-            pyxel_pull.append(y)
-
-
-print(pyxel_pull)
-
+filename = 'pi_digits.txt'
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
