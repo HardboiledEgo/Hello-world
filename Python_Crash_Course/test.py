a922d842631508217e5babfe47f24bdd27906b59
HardboiledEgo <51846859+HardboiledEgo@users.noreply.github.com>
2020-08-12 18:24:41 +0300

    гзв

--- Python_Crash_Course/test.py
+++ Python_Crash_Course/test.py
@@ -1,3 +1,14 @@
-first_name = 'ada'
-second_name = 'lovelace'
-print(f"{first_name.title()} {second_name.title()}")#f-ссылки 
+import math
+num_input = int(input('введи десятичное число: '))
+tenet = int(input('введи необходимое основание: '))
+num_output = []
+while num_input > 1:
+    num_output.append(num_input % tenet)
+    if num_input == tenet:
+        num_output.append(1)
+        break
+    else:
+        num_input = num_input // tenet
+num_output.reverse()
+print(num_output)
+input('***')
